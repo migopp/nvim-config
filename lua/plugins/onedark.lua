14230af08d756e5d2a2bf095a3d037f2e0d21b0d
Michael Goppert <goppert@utexas.edu>
2024-01-11 15:31:06 -0600

    Add current state

---
+++ lua/plugins/onedark.lua
@@ -0,0 +1,14 @@
+return {
+    {
+        "rebelot/kanagawa.nvim",
+        lazy = false,
+        priority = 1000,
+        opts = { style = "lotus" },
+    },
+    {
+        "LazyVim/LazyVim",
+        opts = {
+            colorscheme = "kanagawa",
+        },
+    },
+}
