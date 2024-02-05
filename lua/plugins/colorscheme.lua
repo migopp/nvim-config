e874e905df2c143bf622774ba3591497d40e0580
Michael Goppert <migopp.cs@gmail.com>
2024-02-04 18:31:26 -0600

    Become gruvbox chad

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,17 @@
+return {
+    {
+        "ellisonleao/gruvbox.nvim",
+        lazy = false,
+        priority = 1000,
+        opts = {
+            transparent_mode = true,
+        },
+    },
+    {
+        "LazyVim/LazyVim",
+        opts = {
+            colorscheme = "gruvbox",
+            background_color = "#000000",
+        },
+    },
+}
