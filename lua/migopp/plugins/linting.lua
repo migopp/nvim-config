dd1055e5c4848b24e487465d25a6ef9ba3421d73
Michael Goppert <migopp@mac-btw.local>
2024-08-18 14:24:40 -0500

    Remove pylint

--- lua/migopp/plugins/linting.lua
+++ lua/migopp/plugins/linting.lua
@@ -8,7 +8,6 @@ return {
 		local lint = require("lint")
 
 		lint.linters_by_ft = {
-			python = { "pylint" },
 			javascript = { "eslint_d" },
 			typescript = { "eslint_d" },
 			svelte = { "eslint_d" },
